76011d03f78fea9cfa200a0e7312e5fc5de66ebc
shahzaib irfan <shahzaibirfan1012@gmail.com>
2023-07-28 16:13:43 +0500

    Third Commit

--- app.js
+++ app.js
@@ -25,7 +25,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://blogs-front-end.vercel.app/"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Header", "Centent-Types");
   next();
